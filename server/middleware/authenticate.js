dab92c6cd20e83645bad462c373e65d08c54d669
Joshua Ward <jward@bluemodus.com>
2020-02-25 19:44:37 -0700

    add mongo db dreds

--- server/middleware/authenticate.js
+++ server/middleware/authenticate.js
@@ -5,7 +5,7 @@ let authenticate = (req, res, next) => {
 	let token = req.cookies.auth;
 	User.findByToken(token, (err, user) => {
 		if(err) throw err;
-		if(!user) return res.status(401).send('Bad Request');
+		if(!user) return res.status(401).send(err);
 		req.user = user;
 		req.token = token;
 		next();
